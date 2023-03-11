d00dd10e13b33557b5e3c47cdcc116f8bbdc0189
Rafael Jáber <rafael.jaber@hotmail.com>
2023-03-11 00:54:13 -0300

    Adicionando a documentação na API

--- FilmesApi/Controllers/MovieController.cs
+++ FilmesApi/Controllers/MovieController.cs
@@ -21,6 +21,13 @@ namespace FilmesApi.Controllers
         }
 
         
+        /// <summary>
+        /// Retorna os filmes cadastrados no banco
+        /// </summary>
+        /// <param name="skip">Itens a serem ignorados</param>
+        /// <param name="take">Quantidades de itens para trazer</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso retorne com sucesso</response>
         [HttpGet]
         public IEnumerable<ReadMovieDto> GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 15)
         {
@@ -30,6 +37,13 @@ namespace FilmesApi.Controllers
                     );
         }
 
+        
+        /// <summary>
+        /// Busca um filme especifico atravez do GUID
+        /// </summary>
+        /// <param name="uid">UID referente ao filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso retorne com sucesso</response>
         [HttpGet("{uid:guid}")]
         public IActionResult GetMovie(Guid uid)
         {
@@ -39,7 +53,15 @@ namespace FilmesApi.Controllers
             return Ok(movieDto);
         }
 
+        
+        /// <summary>
+        /// Adiciona um filme ao banco de dados
+        /// </summary>
+        /// <param name="movieDto">Objeto com os campos necessários para criação de um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public IActionResult AddMovie([FromBody] CreateMovieDto movieDto)
         {
             Movie movie = _mapper.Map<Movie>(movieDto);
@@ -48,6 +70,14 @@ namespace FilmesApi.Controllers
             return CreatedAtAction(nameof(GetMovie), new { uid = movie.Id }, movie);
         }
 
+        
+        /// <summary>
+        /// Atualiza um filme no banco de dados
+        /// </summary>
+        /// <param name="uid">UID referente ao filme</param>
+        /// <param name="movieDto">Objeto com os campos necessários para atualizar um filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso a atualização seja feita com sucesso</response>
         [HttpPut("{uid:guid}")]
         public IActionResult UpdateMovie(Guid uid, [FromBody] UpdateMovieDto movieDto)
         {
@@ -59,6 +89,14 @@ namespace FilmesApi.Controllers
             return NoContent();
         }
         
+        
+        /// <summary>
+        /// Atualiza um um parametro do filme no banco de dados
+        /// </summary>
+        /// <param name="uid">UID referente ao filme</param>
+        /// <param name="patch">Objeto com os campos necessários para atualizar um filme por Path</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso a atualização seja feita com sucesso</response>
         [HttpPatch("{uid:guid}")]
         public IActionResult UpdateMoviePatch(Guid uid, JsonPatchDocument<UpdateMovieDto> patch)
         {
@@ -76,6 +114,13 @@ namespace FilmesApi.Controllers
             return NoContent();
         }
 
+        
+        /// <summary>
+        /// Realiza o delete de um filme no banco de dados
+        /// </summary>
+        /// <param name="uid">UID referente ao filme</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="204">Caso o detele seja feita com sucesso</response>
         [HttpDelete("{uid:guid}")]
         public IActionResult DeleteMovie(Guid uid)
         {
